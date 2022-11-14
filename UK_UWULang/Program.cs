1906a5b5c6f88a09d29e40310f2338613b16919b
TeamDoodz <unconfigured@null.spigotmc.org>
2022-11-14 17:41:24 -0600

    Add project files.

---
+++ UK_UWULang/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace UK_UWULang;
+
+internal class Program {
+	static readonly string[] localeCategories = new string[] {
+		"credits",
+		"style",
+		"cyberGrind",
+		"books",
+		"intermission",
+		"frontend",
+		"visualnovel",
+		"options",
+		"pauseMenu",
+		"levelNames",
+		"levelChallenges",
+		"levelTips",
+		"shop",
+		"enemyNames",
+		"cheats",
+		"enemybios",
+		"tutorial",
+		"prelude",
+		"act1",
+		"act2",
+		"primeSanctum",
+		"secretLevels",
+		"subtitles",
+		"misc"
+	};
+
+	static void Main(string[] args) {
+		var serializer = new JsonSerializer();
+		serializer.Formatting = Formatting.Indented;
+
+		CreateLang(serializer, 0);
+		CreateLang(serializer, 1);
+		CreateLang(serializer, 2);
+	}
+
+	private static void CreateLang(JsonSerializer serializer, int intensity) {
+		TextReader templateReader = new StreamReader(File.OpenRead("template.json"));
+		JObject lang = serializer.Deserialize<JObject>(new JsonTextReader(templateReader)) ?? throw new Exception("Failed to deserialize template.");
+
+		try {
+			lang["metadata"]["langName"] = $"uwulang_{intensity}";
+			lang["metadata"]["langAuthor"] = "TeamDoodz";
+			lang["metadata"]["langDisplayName"] = $"UwULang {intensity+1}";
+			lang["body"]["bodyName"] = UWUIfier.UWUIfy("Converts all of Ultrakill's strings into UWUspeak.", intensity);
+		} catch(Exception ex) {
+			Console.WriteLine($"There was an error writing language metadata: {ex}");
+			return;
+		}
+
+		foreach(string key in localeCategories) {
+			if(!lang.ContainsKey(key)) continue;
+			JToken? child = lang[key];
+			if(child is not JObject obj) continue;
+			ConvertAll(obj, intensity);
+		}
+
+		using(StreamWriter sw = File.CreateText($"uwulang_{intensity}.json")) {
+			serializer.Serialize(new JsonTextWriter(sw), lang);
+		}
+	}
+
+	static void ConvertAll(JObject obj, int intensity) {
+		List<KeyValuePair<string, string>> toModify = new();
+		foreach(var pair in obj) {
+			toModify.Add(new(pair.Key, UWUIfier.UWUIfy(pair.Value?.ToString() ?? "null", intensity)));
+		}
+		foreach(var pair in toModify) {
+			obj[pair.Key] = pair.Value;
+		}
+	}
+}
---
+++ UK_UWULang/UwUIfier.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace UK_UWULang;
+
+public static partial class UWUIfier {
+	const string UV = "uv";
+	const string W = "w";
+	const string UWU = "uwu";
+	const string OWO = "owo";
+
+	public static string UWUIfy(string text, int intensity) {
+		string outp = DoRegex(text, intensity);
+		return outp;
+	}
+
+	private static string DoRegex(string text, int intensity) {
+		string outp = text.ToLower();
+		outp = OveRegex().Replace(outp, UV);
+		outp = RAndLRegex().Replace(outp, W);
+		outp = VowelRegex().Replace(outp, W);
+		if(intensity >= 1) outp = NoRLBeforeWRegex().Replace(outp, string.Empty);
+		if(intensity >= 1) outp = NoRLAfterWRegex().Replace(outp, string.Empty);
+		if(intensity >= 2) outp = IncompleteUwuRegex().Replace(outp, UWU);
+		if(intensity >= 2) outp = IncompleteOwoRegex().Replace(outp, OWO);
+		return outp;
+	}
+
+	[GeneratedRegex(@"(?<!<)(ove)|(of)(?![\w\d\s=]*>)")]
+	private static partial Regex OveRegex();
+
+	[GeneratedRegex(@"(?<!<)(?<![lrou])[lr](?![\w\d\s=]*>)")]
+	private static partial Regex RAndLRegex();
+
+	[GeneratedRegex(@"(?<!<)(?<!\b|[waoug])(?=[aou](?!\b|w|[aeiou]))(?![\w\d\s=]*>)")]
+	private static partial Regex VowelRegex();
+
+	[GeneratedRegex(@"(?<!<)[lr](?>w)(?![\w\d\s=]*>)")]
+	private static partial Regex NoRLBeforeWRegex();
+
+	[GeneratedRegex(@"(?<!<)(?<=w)[lr](?![\w\d\s=]*>)")]
+	private static partial Regex NoRLAfterWRegex();
+
+	[GeneratedRegex(@"(?<!<)((?<!u)wu)|(uw(?!u))(?![\w\d\s=]*>)")]
+	private static partial Regex IncompleteUwuRegex();
+
+	[GeneratedRegex(@"(?<!<)((?<!o)wo)|(ow(?!o))(?![\w\d\s=]*>)")]
+	private static partial Regex IncompleteOwoRegex();
+}
